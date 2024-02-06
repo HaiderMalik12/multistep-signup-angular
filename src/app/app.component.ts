a249672721ea9b619c925f2b8cef3eaa22e6567f
HaiderMalik12 <haidermalik504@gmail.com>
2024-02-06 17:09:12 +0500

    created account component successfully!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AccountComponent } from './account/account.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [AccountComponent, RouterOutlet],
 })
 export class AppComponent {
   title = 'signup-task';
