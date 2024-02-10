c9e3504c139cb70256965d6fbe7378f697ce0852
HaiderMalik12 <haidermalik504@gmail.com>
2024-02-10 22:02:23 +0500

    added about component

---
+++ src/app/about-form.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-form',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      about-form works!
+    </p>
+  `,
+  styles: ``
+})
+export class AboutFormComponent {
+
+}
