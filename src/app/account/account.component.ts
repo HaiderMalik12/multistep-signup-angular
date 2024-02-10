41215b4d041e1ae430d4bd9356a52d2db8a61377
HaiderMalik12 <haidermalik504@gmail.com>
2024-02-10 21:15:31 +0500

    implemented form for first page

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-account',
@@ -9,4 +9,14 @@ import { ReactiveFormsModule } from '@angular/forms';
   templateUrl: './account.component.html',
   styleUrl: './account.component.css',
 })
-export class AccountComponent {}
+export class AccountComponent {
+  constructor(private formBuilder: FormBuilder) {}
+  signupForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  onSubmit() {
+    console.warn(this.signupForm.value);
+  }
+}
