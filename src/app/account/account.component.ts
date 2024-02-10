cc75f4324620eba6cb248978ccb890a992eb2ec8
HaiderMalik12 <haidermalik504@gmail.com>
2024-02-10 21:08:22 +0500

    import formsmodule

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ReactiveFormsModule],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css',
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AccountComponent } from './account/account.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AccountComponent, RouterOutlet],
+  imports: [AccountComponent, RouterOutlet, ReactiveFormsModule],
 })
 export class AppComponent {
   title = 'signup-task';
