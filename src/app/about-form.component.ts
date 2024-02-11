f852a47edec7ddf3b858b14b0fdc92094db9aa14
HaiderMalik12 <haidermalik504@gmail.com>
2024-02-12 02:27:39 +0500

    setup form

--- src/app/about-form.component.ts
+++ src/app/about-form.component.ts
@@ -5,12 +5,32 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      about-form works!
-    </p>
-  `,
-  styles: ``
-})
-export class AboutFormComponent {
+    <div class="container mt-5">
+      <div class="row">
+        <h2 class="about-title text-center">Tell us about yourself</h2>
+      </div>
+      <div class="row">
+        <p class="text-center sub-title">
+          Welcome! Let's get to know each other a bit better.
+        </p>
+      </div>
+      <form>
+        <div class="row align-items-center justify-content-center ms-3 me-1">
+          <div class="col-md-8 col-lg-5 col-sm-12 mb-3">
+            <input
+              type="text"
+              class="form-control"
+              id="fullname"
+              aria-describedby="fullname"
+              placeholder="Full name"
+            />
+          </div>
+        </div>
 
-}
+        <!-- <button type="submit" class="btn btn-primary">Submit</button> -->
+      </form>
+    </div>
+  `,
+  styleUrl: './about-form.component.css',
+})
+export class AboutFormComponent {}
