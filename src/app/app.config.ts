e03080c887e7a6ed4756ac37914eba22ec5cf6bb
HaiderMalik12 <haidermalik504@gmail.com>
2024-02-13 00:59:53 +0500

    remove ngx mask

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideEnvironmentNgxMask } from 'ngx-mask';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideEnvironmentNgxMask()],
+  providers: [provideRouter(routes)],
 };
